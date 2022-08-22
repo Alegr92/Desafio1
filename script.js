144d11dbcecf51d80167e1dd6268f2e65531b594
Alejandro Ruiz <ale.gr92@yahoo.com.ar>
2022-08-22 19:21:14 -0300

    Correccion del trabajo

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-let menu = parseInt(prompt(`1- Ingresar nueva nota de examen \n0- Salir`));
+let menu = parseInt(
+  prompt(`1- Ingresar nueva nota de examen \n2- Ver Promedios\n0- Salir`)
+);
 let cont = 0;
 let suma = 0;
 
-if (menu !== 0 && menu !== 1) {
-  alert(`Opcion no valida, ingrese otra opcion`);
-  menu = parseInt(prompt(`1- Ingresar nueva nota de examen \n0- Salir`));
+if (menu !== 0 && menu !== 1 && menu !== 2) {
+  while (menu !== 0 && menu !== 1 && menu !== 2) {
+    alert(`Opcion no valida, ingrese otra opcion`);
+    menu = parseInt(
+      prompt(`1- Ingresar nueva nota de examen \n2-Ver Promedios\n0- Salir`)
+    );
+  }
+}
+
+if (menu === 2) {
+  while (menu === 2) {
+    alert(`Primero ingrese una nota`);
+    menu = parseInt(
+      prompt(`1- Ingresar nueva nota de examen \n2-Ver Promedios\n0- Salir`)
+    );
+  }
 }
 
 while (menu !== 0) {
@@ -17,13 +32,17 @@ while (menu !== 0) {
   if (menu === 2) {
     console.log(`El promedio de las notas fue ${suma / cont}`);
   }
+
   menu = parseInt(
-    prompt(`1- Ingresar nota del examen\n2-Ver Promedio \n0- Salir`)
+    prompt(`1- Ingresar nota del examen\n2- Ver Promedio \n0- Salir`)
   );
+
   if (menu !== 0 && menu !== 1 && menu !== 2) {
-    alert(`Opcion no valida, ingrese otra opcion`);
-    menu = parseInt(
-      prompt(`1- Ingresar nota del examen\n2-Ver Promedio \n0- Salir`)
-    );
+    while (menu !== 0 && menu !== 1 && menu !== 2) {
+      alert(`Opcion no valida, ingrese otra opcion`);
+      menu = parseInt(
+        prompt(`1- Ingresar nota del examen\n2- Ver Promedio \n0- Salir`)
+      );
+    }
   }
 }
